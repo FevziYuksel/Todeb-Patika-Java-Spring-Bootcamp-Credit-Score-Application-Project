7a99b51562ab563e750880eabcd9c1801ff7613b
Fevzi Yüksel <fevziyuksel1996@gmail.com>
2022-08-02 18:16:09 +0300

    Initial Commit

---
+++ src/test/java/com/todebpatikajavaspringbootcampcreditscoreapplicationproject/TodebPatikaJavaSpringBootcampCreditScoreApplicationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todebpatikajavaspringbootcampcreditscoreapplicationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodebPatikaJavaSpringBootcampCreditScoreApplicationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
