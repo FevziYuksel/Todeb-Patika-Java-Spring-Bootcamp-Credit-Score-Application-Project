5389e855eb296aae9c4e23eb84df94e6628b87a6
Fevzi Yüksel <fevziyuksel1996@gmail.com>
2022-08-04 19:10:02 +0300

    Add AlreadyExistsException.java

---
+++ src/main/java/com/todebpatikajavaspringbootcampcreditscoreapplicationproject/exception/AlreadyExistsException.java
@@ -0,0 +1,23 @@
+package com.todebpatikajavaspringbootcampcreditscoreapplicationproject.exception;
+
+
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+@Data
+@ResponseStatus(HttpStatus.CONFLICT)
+public class AlreadyExistsException extends RuntimeException {
+
+    private String details;
+
+    public AlreadyExistsException(String entityName, String nationalId, String cause) {
+        super(String.format("Related %s by national ID %s already exists with : [%s]",entityName,cause,nationalId) );
+        details = super.getLocalizedMessage();
+
+    }
+    public AlreadyExistsException(String entityName, String cause) {
+        super(String.format("Related %s already exists with : [%s]",entityName,cause));
+        details = super.getLocalizedMessage();
+    }
+}
