a0e168639b1d22ebdaeb8bdbe7eb287729493ba8
Fevzi Yüksel <fevziyuksel1996@gmail.com>
2022-08-12 00:10:58 +0300

    Add CreditService

--- src/main/java/com/todebpatikajavaspringbootcampcreditscoreapplicationproject/service/impl/CreditService.java
+++ src/main/java/com/todebpatikajavaspringbootcampcreditscoreapplicationproject/service/impl/CreditService.java
@@ -2,6 +2,7 @@ package com.todebpatikajavaspringbootcampcreditscoreapplicationproject.service.i
 
 import com.todebpatikajavaspringbootcampcreditscoreapplicationproject.model.entity.Credit;
 import com.todebpatikajavaspringbootcampcreditscoreapplicationproject.model.entity.CreditApplication;
+import com.todebpatikajavaspringbootcampcreditscoreapplicationproject.model.entity.Customer;
 import com.todebpatikajavaspringbootcampcreditscoreapplicationproject.repository.CreditRepository;
 import com.todebpatikajavaspringbootcampcreditscoreapplicationproject.service.ICreditService;
 import lombok.AllArgsConstructor;
@@ -13,17 +14,25 @@ public class CreditService implements ICreditService {
 
     private final CreditRepository creditRepository;
 
+    private final NotificationService notificationService;
+
 
 
     @Override
     public Credit createCredit(CreditApplication creditApplication) {
         //add dublicated check and if there is an active application
-        //add to customer
+
         Credit credit = new Credit();
         credit.setCreditLimit(creditApplication.getCreditLimit());
         credit.setCreditStatus(creditApplication.getApplicationStatus());
+        credit.setCustomer(creditApplication.getCustomer());
 
+        notificationService.sendNotificationMessage(creditApplication);
         return creditRepository.save(credit);
+    }
+    private boolean hasAnyActiveCredit(Customer customer){
+        return customer.getCreditApplications().stream().anyMatch(credit -> credit.getApplicationStatus().getIsActive());
+
     }
 
     @Override
