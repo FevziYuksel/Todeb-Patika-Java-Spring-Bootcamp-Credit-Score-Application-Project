b622c78f15b0b1e5c07d18082c85dd600ba5927b
Fevzi Yüksel <fevziyuksel1996@gmail.com>
2022-08-12 21:32:30 +0300

    Refactor CreditApplicationRepository

--- src/main/java/com/todebpatikajavaspringbootcampcreditscoreapplicationproject/repository/CreditApplicationRepository.java
+++ src/main/java/com/todebpatikajavaspringbootcampcreditscoreapplicationproject/repository/CreditApplicationRepository.java
@@ -3,9 +3,10 @@ package com.todebpatikajavaspringbootcampcreditscoreapplicationproject.repositor
 import com.todebpatikajavaspringbootcampcreditscoreapplicationproject.model.entity.CreditApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.Optional;
 
 public interface CreditApplicationRepository extends JpaRepository<CreditApplication, Long> {
-
 
 }
